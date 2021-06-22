6ef3296197e09fa9a97ea614bd755bd5ca81284f
ryan <ball.ryan.rb@gmail.com>
2021-06-22 11:58:31 -0600

    adds collections and records index to profile page

---
+++ src/views/Profile.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="profile">
+    <h1>{{ message }}</h1>
+    <h3>Collections:</h3>
+    <div v-for="collection in collections" v-bind:key="collection.id">
+      <p>
+        {{ collection.name }} <br />
+        {{ collection.partners }} <br />
+        {{ collection.highlights }} <br />
+      </p>
+    </div>
+    <h3>Route Records:</h3>
+    <div v-for="record in records" v-bind:key="record.id">
+      <p>
+        {{ record.date }} <br />
+        {{ record.route.name }} <br />
+        {{ record.route.location }} <br />
+        {{ record.grade }} <br />
+        {{ record.result }} <br />
+      </p>
+      <a :href="record.route.mp_url" target="_blank">MP URL</a>
+    </div>
+  </div>
+</template>
+
+<style></style>
+
+<script>
+import axios from "axios";
+export default {
+  data: function () {
+    return {
+      message: "Welcome to your profile page!",
+      collections: [],
+      records: [],
+    };
+  },
+  created: function () {
+    axios.get("/collections").then((response) => {
+      console.log(response.data);
+      this.collections = response.data;
+    });
+    axios.get("/records").then((response) => {
+      console.log(response.data);
+      this.records = response.data;
+    });
+  },
+  methods: {},
+};
+</script>
