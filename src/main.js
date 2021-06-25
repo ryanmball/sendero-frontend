35323db07861e805cec95f10b3a305bbc025a941
ryan <ball.ryan.rb@gmail.com>
2021-06-24 20:04:52 -0600

    adds record create action to modal on records index

--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import router from "./router";
 import axios from "axios";
 import "bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
+import "pikaday";
 
 axios.defaults.baseURL =
   process.env.NODE_ENV === "development" ? "http://localhost:3000" : "/";
--- src/views/Records.vue
+++ src/views/Records.vue
@@ -19,10 +19,8 @@
         data-bs-backdrop="static"
         data-bs-keyboard="false"
         tabindex="-1"
-        aria-labelledby="staticBackdropLabel"
-        aria-hidden="true"
       >
-        <div class="modal-dialog">
+        <div class="modal-dialog modal-lg">
           <div class="modal-content">
             <div class="modal-header">
               <h5 class="modal-title" id="staticBackdropLabel">
@@ -32,10 +30,144 @@
                 type="button"
                 class="btn-close"
                 data-bs-dismiss="modal"
-                aria-label="Close"
               ></button>
             </div>
-            <div class="modal-body">...</div>
+            <div class="modal-body">
+              <div class="container-fluid">
+                <div class="row">
+                  <div class="col-md-4">
+                    <div class="input-group mb-3">
+                      <!-- <input type="text" id="datepicker" name="prevent_autofill" /> ADD DATE PICKER -->
+                      <span
+                        class="input-group-text"
+                        id="inputGroup-sizing-default"
+                        >Date</span
+                      >
+                      <input
+                        type="text"
+                        class="form-control"
+                        v-model="newRecordParams.date"
+                      />
+                    </div>
+                  </div>
+                  <div class="col-md-4 ms-auto">
+                    <div class="input-group mb-3">
+                      <span
+                        class="input-group-text"
+                        id="inputGroup-sizing-default"
+                        >Partner</span
+                      >
+                      <input
+                        type="text"
+                        class="form-control"
+                        v-model="newRecordParams.partner"
+                      />
+                    </div>
+                  </div>
+                </div>
+                <div class="row">
+                  <div class="col-md-8">
+                    <div class="input-group mb-3">
+                      <span
+                        class="input-group-text"
+                        id="inputGroup-sizing-default"
+                        >Route</span
+                      >
+                      <!-- ADD AUTOCOMPLETE FROM FULL ROUTE LISTING IN DATABASE -->
+                      <input
+                        type="text"
+                        class="form-control"
+                        v-model="newRecordParams.route_id"
+                      />
+                    </div>
+                  </div>
+                  <div class="col-md-6">
+                    <div class="input-group mb-3">
+                      <span
+                        class="input-group-text"
+                        id="inputGroup-sizing-default"
+                        >Crag</span
+                      >
+                      <input type="text" class="form-control" />
+                    </div>
+                  </div>
+                  <div class="col-md-6">
+                    <div class="input-group mb-3">
+                      <span
+                        class="input-group-text"
+                        id="inputGroup-sizing-default"
+                        >Area</span
+                      >
+                      <input type="text" class="form-control" />
+                    </div>
+                  </div>
+                  <div class="col-md-3">
+                    <div class="input-group mb-3">
+                      <span
+                        class="input-group-text"
+                        id="inputGroup-sizing-default"
+                        >Grade</span
+                      >
+                      <input
+                        type="text"
+                        class="form-control"
+                        v-model="newRecordParams.grade"
+                      />
+                    </div>
+                  </div>
+                  <div class="col-md-5">
+                    <div class="input-group mb-3">
+                      <span
+                        class="input-group-text"
+                        id="inputGroup-sizing-default"
+                        >Result</span
+                      >
+                      <input
+                        type="text"
+                        class="form-control"
+                        v-model="newRecordParams.result"
+                      />
+                    </div>
+                  </div>
+                  <div class="col-md-4">
+                    <div class="input-group mb-3">
+                      <span
+                        class="input-group-text"
+                        id="inputGroup-sizing-default"
+                        >In Progress</span
+                      >
+                      <input
+                        type="text"
+                        class="form-control"
+                        v-model="newRecordParams.in_progress"
+                      />
+                    </div>
+                    <div class="input-group mb-3">
+                      <span
+                        class="input-group-text"
+                        id="inputGroup-sizing-default"
+                        >Rating</span
+                      >
+                      <input
+                        type="text"
+                        class="form-control"
+                        v-model="newRecordParams.rating"
+                      />
+                    </div>
+                  </div>
+                  <div class="col-md-12">
+                    <div class="input-group">
+                      <span class="input-group-text">Comments</span>
+                      <textarea
+                        class="form-control"
+                        v-model="newRecordParams.comments"
+                      ></textarea>
+                    </div>
+                  </div>
+                  <!-- CREATE ADD TO COLLECTION OPTION -->
+                </div>
+              </div>
+            </div>
             <div class="modal-footer">
               <button
                 type="button"
@@ -44,7 +176,13 @@
               >
                 Close
               </button>
-              <button type="button" class="btn btn-primary">Save</button>
+              <button
+                type="button"
+                class="btn btn-primary"
+                @click="recordCreate()"
+              >
+                Save
+              </button>
             </div>
           </div>
         </div>
@@ -78,6 +216,8 @@ export default {
   data: function () {
     return {
       records: [],
+      newRecordParams: {},
+      errors: [],
     };
   },
   created: function () {
@@ -86,6 +226,17 @@ export default {
       this.records = response.data;
     });
   },
-  methods: {},
+  methods: {
+    recordCreate: function () {
+      axios
+        .post("/records", this.newRecordParams)
+        .then((response) => {
+          console.log(response.data);
+        })
+        .catch((error) => {
+          this.errors = error.response.data.errors;
+        });
+    },
+  },
 };
 </script>
