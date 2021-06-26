3207886abc577c45dcdb5de8819dd3d8317e786d
ryan <ball.ryan.rb@gmail.com>
2021-06-26 12:00:46 -0600

    adds collection create, show, edit, destroy - still need to test all

--- src/views/Collections.vue
+++ src/views/Collections.vue
@@ -2,18 +2,89 @@
   <div class="collections-index">
     <div class="container">
       <h3>Collections:</h3>
+
+      <!-- Collection Create -->
+      <form v-on:submit.prevent="collectionCreate()">
+        <ul style="list-style-type: none">
+          <li class="text-danger" v-for="error in errors" v-bind:key="error">
+            {{ error }}
+          </li>
+        </ul>
+        <small class="text-danger">This will be in a modal</small>
+        <div class="form-group">
+          <label>Name:</label>
+          <input
+            type="text"
+            class="form-control"
+            v-model="newCollectionParams.name"
+          />
+        </div>
+        <div class="form-group">
+          <label>Partners:</label>
+          <input
+            type="text"
+            class="form-control"
+            v-model="newCollectionParams.partners"
+          />
+        </div>
+        <div class="form-group">
+          <label>Highlights:</label>
+          <input
+            type="text"
+            class="form-control"
+            v-model="newCollectionParams.highlights"
+          />
+        </div>
+        <strong>UPLOAD IMAGES</strong> <br />
+        <input type="submit" class="btn btn-primary" value="Create" />
+      </form>
+
+      <!-- Collection Index -->
       <div v-for="collection in collections" v-bind:key="collection.id">
         <p>
           {{ collection.name }} <br />
           {{ collection.partners }} <br />
           {{ collection.highlights }} <br />
+          <button v-on:click="collectionShow(collection)">Edit/Delete</button>
         </p>
       </div>
+
+      <!-- Collection Edit/Delete -->
+      <dialog id="collection-details">
+        <form method="dialog">
+          <h1>Edit Collection</h1>
+          <ul style="list-style-type: none">
+            <li
+              class="text-danger"
+              v-for="editError in editErrors"
+              v-bind:key="editError"
+            >
+              {{ error }}
+            </li>
+          </ul>
+          <p>Name: <input type="text" v-model="editCollectionParams.name" /></p>
+          <p>
+            Partners:
+            <input type="text" v-model="editCollectionParams.partners" />
+          </p>
+          <p>
+            Highlights:
+            <input type="text" v-model="editCollectionParams.highlights" />
+          </p>
+          <button v-on:click="collectionUpdate()">Update</button>
+          <button v-on:click="collectionDestroy()">Delete</button>
+          <button>Close</button>
+        </form>
+      </dialog>
     </div>
   </div>
 </template>
 
-<style></style>
+<style>
+.text-danger {
+  color: red;
+}
+</style>
 
 <script>
 import axios from "axios";
@@ -24,6 +95,10 @@ export default {
   data: function () {
     return {
       collections: [],
+      newCollectionParams: {},
+      errors: [],
+      editCollectionParams: {},
+      editErrors: [],
     };
   },
   created: function () {
@@ -32,6 +107,48 @@ export default {
       this.collections = response.data;
     });
   },
-  methods: {},
+  methods: {
+    collectionCreate: function () {
+      axios
+        .post("/collections", this.newCollectionParams)
+        .then((response) => {
+          console.log(response.data);
+          this.collections.push(response.data);
+          this.newCollectionParams = {};
+        })
+        .catch((error) => {
+          this.errors = error.response.data.errors;
+        });
+    },
+    collectionShow: function (collection) {
+      console.log(collection);
+      this.editCollectionParams = collection;
+      document.querySelector("#collection-details").showModal();
+    },
+    collectionUpdate: function () {
+      axios
+        .patch(
+          `/collections/${this.editCollectionParams.id}`,
+          this.editCollectionParams
+        )
+        .then((response) => {
+          console.log(response.data);
+          var index = this.collections.indexOf(this.editCollectionParams);
+          this.collections.splice(index, 1, response.data);
+        })
+        .catch((error) => {
+          this.editErrors = error.response.data.errors;
+        });
+    },
+    collectionDestroy: function () {
+      axios
+        .delete(`/collections/${this.editCollectionParams.id}`)
+        .then((response) => {
+          console.log(response.data);
+          var index = this.collections.indexOf(this.editCollectionParams);
+          this.collections.splice(index, 1);
+        });
+    },
+  },
 };
 </script>
