c9670419b2fb6a8a79676a8d86318ead23b3dd1c
ryan <ball.ryan.rb@gmail.com>
2021-07-01 12:43:35 -0600

    adds image upload to collections index

--- src/views/Collections.vue
+++ src/views/Collections.vue
@@ -2,150 +2,89 @@
   <div class="collections-index">
     <div class="container">
       <!-- Collection Create -->
-      <form v-on:submit.prevent="collectionCreate()">
-        <ul style="list-style-type: none">
-          <li class="text-danger" v-for="error in errors" v-bind:key="error">
-            {{ error }}
-          </li>
-        </ul>
-        <strong>Create collection</strong><br />
-        <small class="text-danger">This will be in a modal</small>
-        <div class="form-group">
-          <label>Name:</label>
-          <input
-            type="text"
-            class="form-control"
-            v-model="newCollectionParams.name"
-          />
-        </div>
-        <div class="form-group">
-          <label>Partners:</label>
-          <input
-            type="text"
-            class="form-control"
-            v-model="newCollectionParams.partners"
-          />
-        </div>
-        <div class="form-group">
-          <label>Highlights:</label>
-          <input
-            type="text"
-            class="form-control"
-            v-model="newCollectionParams.highlights"
-          />
-        </div>
-        <input type="submit" class="btn btn-primary" value="Create" />
-        <br />
-      </form>
-      <br />
-      <small class="text-danger">This will be in a second modal</small>
-      <br />
-      <strong>Upload images</strong> <br />
-      <input type="text" v-model="newImageParams.url" placeholder="Photo URL" />
-      <br />
-      <select v-model="collection">
-        <option
-          v-for="collection in collections"
-          v-bind:key="collection.id"
-          :value="collection"
-        >
-          {{ collection.name }}
-        </option>
-      </select>
-      <br />
-      <button @click="imageCreate(collection)">Upload Photo</button> <br />
-      <small class="text-danger">
-        After creating a collection it will open another modal where the user
-        <br />
-        will upload images. This will use the collection id from the collection
-        <br />
-        create response data. Dropdown is temporary
-      </small>
-
-      <!-- Collection Index -->
-      <h3>Collections:</h3>
-      <div v-for="collection in collections" v-bind:key="collection.id">
-        <p>
-          <strong>Name: </strong>{{ collection.name }} <br />
-          <strong>Partners: </strong>{{ collection.partners }} <br />
-          <strong>Highlights: </strong>{{ collection.highlights }} <br />
-          <router-link :to="`/collections/${collection.id}`">
-            <button>More</button>
-          </router-link>
-        </p>
-      </div>
-
-      <!-- Collection Records -->
-      <dialog id="collection-records">
-        <form method="dialog">
-          <h3>{{ collection.name }} Routes</h3>
-          <div
-            v-for="record in orderBy(records, 'date', -1)"
-            v-bind:key="record.id"
-          >
-            <p>
-              <strong>Date: </strong>{{ record.date }} <br />
-              <strong>Route: </strong>{{ record.route.name }} <br />
-              <strong>Location: </strong>{{ record.route.location }}<br />
-              <strong>Grade: </strong>{{ record.grade }} <br />
-              <strong>Partner: </strong>{{ record.partner }} <br />
-              <strong>Comments: </strong>{{ record.comments }} <br />
-
-              <!-- replace this link with a small MP logo -->
-              <a :href="record.route.mp_url" target="_blank">MP URL</a
-              ><small class="text-danger">small MP logo for link</small> <br />
-            </p>
-          </div>
-          <button>Close</button>
-        </form>
-      </dialog>
-
-      <!-- Collection Images -->
-      <dialog id="collection-images">
-        <form method="dialog">
-          <h3>{{ collection.name }} Photos</h3>
-          <input
-            type="text"
-            v-model="newImageParams.url"
-            placeholder="Photo URL"
-          />
-          <button @click="imageCreate(collection)">Upload Photo</button>
-          <div v-for="image in images" v-bind:key="image.id">
-            <br />
-            <img class="image" :src="image.url" alt="photo" />
-            <button @click="imageDestroy(image)">Delete Photo</button>
-          </div>
-          <button>Close</button>
-        </form>
-      </dialog>
-
-      <!-- Collection Edit/Delete -->
-      <dialog id="collection-details">
-        <form method="dialog">
-          <h1>Edit Collection</h1>
+      <div>
+        <form v-on:submit.prevent="collectionCreate()">
           <ul style="list-style-type: none">
-            <li
-              class="text-danger"
-              v-for="editError in editErrors"
-              v-bind:key="editError"
-            >
+            <li class="text-danger" v-for="error in errors" v-bind:key="error">
               {{ error }}
             </li>
           </ul>
-          <p>Name: <input type="text" v-model="editCollectionParams.name" /></p>
-          <p>
-            Partners:
-            <input type="text" v-model="editCollectionParams.partners" />
-          </p>
-          <p>
-            Highlights:
-            <input type="text" v-model="editCollectionParams.highlights" />
-          </p>
-          <button v-on:click="collectionUpdate()">Update</button>
-          <button v-on:click="collectionDestroy()">Delete</button>
-          <button>Close</button>
+          <strong>Create collection</strong><br />
+          <small class="text-danger">This will be in a modal</small>
+          <div class="form-group">
+            <label>Name:</label>
+            <input
+              type="text"
+              class="form-control"
+              v-model="newCollectionParams.name"
+            />
+          </div>
+          <div class="form-group">
+            <label>Partners:</label>
+            <input
+              type="text"
+              class="form-control"
+              v-model="newCollectionParams.partners"
+            />
+          </div>
+          <div class="form-group">
+            <label>Highlights:</label>
+            <input
+              type="text"
+              class="form-control"
+              v-model="newCollectionParams.highlights"
+            />
+          </div>
+          <input type="submit" class="btn btn-primary" value="Create" />
+          <br />
         </form>
-      </dialog>
+        <br />
+      </div>
+
+      <!-- Image Upload (after collection create) -->
+      <div>
+        <small class="text-danger">This will be in a second modal</small>
+        <br />
+        <form v-on:submit.prevent="imageCreate(collection)">
+          <strong>Upload images</strong> <br />
+          <input type="file" v-on:change="setFile($event)" ref="fileInput" />
+          <br />
+          <select v-model="collection">
+            <option
+              v-for="collection in collections"
+              v-bind:key="collection.id"
+              :value="collection"
+            >
+              {{ collection.name }}
+            </option>
+          </select>
+          <br />
+          <input type="submit" value="Upload Photo" />
+        </form>
+        <small class="text-danger">
+          After creating a collection it will open another modal where the user
+          <br />
+          will upload images. This will use the collection id from the
+          collection
+          <br />
+          create response data. Dropdown is temporary
+        </small>
+      </div>
+
+      <!-- Collection Index -->
+      <div>
+        <h3>Collections:</h3>
+        <div v-for="collection in collections" v-bind:key="collection.id">
+          <p>
+            <strong>Name: </strong>{{ collection.name }} <br />
+            <strong>Partners: </strong>{{ collection.partners }} <br />
+            <strong>Highlights: </strong>{{ collection.highlights }} <br />
+            <router-link :to="`/collections/${collection.id}`">
+              <button>More</button>
+            </router-link>
+          </p>
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -171,12 +110,8 @@ export default {
       collections: [],
       newCollectionParams: {},
       errors: [],
-      editCollectionParams: {},
-      editErrors: [],
-      images: [],
-      newImageParams: {},
+      image: "",
       collection: {},
-      records: [],
     };
   },
   created: function () {
@@ -198,75 +133,24 @@ export default {
           this.errors = error.response.data.errors;
         });
     },
-    collectionShow: function (collection) {
-      console.log(collection);
-      this.editCollectionParams = collection;
-      document.querySelector("#collection-details").showModal();
-    },
-    collectionRecords: function (collection) {
-      console.log(collection);
-      this.collection = collection;
-      this.records = collection.records;
-      document.querySelector("#collection-records").showModal();
-    },
-    collectionImages: function (collection) {
-      console.log(collection);
-      this.collection = collection;
-      this.images = collection.images;
-      document.querySelector("#collection-images").showModal();
+    setFile: function (event) {
+      if (event.target.files.length > 0) {
+        this.image = event.target.files[0];
+      }
     },
     imageCreate: function (collection) {
-      this.newImageParams.collection_id = collection.id;
+      var formData = new FormData();
+      formData.append("image", this.image);
+      formData.append("collection_id", collection.id);
       axios
-        .post("/images", this.newImageParams)
+        .post("/images", formData)
         .then((response) => {
           console.log(response.data);
-          this.images.push(response.data);
-          this.newImageParams = {};
           this.collection = {};
         })
         .catch((error) => {
           this.errors = error.response.data.errors;
         });
-    },
-    imageDestroy: function (image) {
-      if (confirm("Are you sure you want to delete this photo?")) {
-        axios.delete(`/images/${image.id}`).then((response) => {
-          console.log(response.data);
-          var index = this.images.indexOf(image);
-          this.images.splice(index, 1);
-        });
-      }
-    },
-    collectionUpdate: function () {
-      axios
-        .patch(
-          `/collections/${this.editCollectionParams.id}`,
-          this.editCollectionParams
-        )
-        .then((response) => {
-          console.log(response.data);
-          var index = this.collections.indexOf(this.editCollectionParams);
-          this.collections.splice(index, 1, response.data);
-        })
-        .catch((error) => {
-          this.editErrors = error.response.data.errors;
-        });
-    },
-    collectionDestroy: function () {
-      if (
-        confirm(
-          "Are you sure you want to delete this collection? \r\nAll associated photos will also be deleted."
-        )
-      ) {
-        axios
-          .delete(`/collections/${this.editCollectionParams.id}`)
-          .then((response) => {
-            console.log(response.data);
-            var index = this.collections.indexOf(this.editCollectionParams);
-            this.collections.splice(index, 1);
-          });
-      }
     },
   },
 };
